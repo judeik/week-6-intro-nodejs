c99f5fba2e3b9b60c003a31b622f4123850980de
judeik <judeojobor@gmail.com>
2025-06-07 23:56:25 +0100

    Intro to Node JS

---
+++ index.js
@@ -0,0 +1,17 @@
+// const a = 4
+// const b = 9
+
+// const answer = a * b
+// console.log(answer);
+
+// IP Address: How you connect to a server. It is listenting to a request that is made through particular port or channel. If you want it to listening to you you have to make that port available. 
+
+// Domain
+
+// Port: Is a channel to connect to your server. It must be secrte
+
+// http -> Insecured
+
+// https -> Secured
+
+// Setup the server Stopped: 1:16:27
